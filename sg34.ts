fd182f703bdd81fb1458f049d20e3cf4f9dbdfe7
KalbeAbbas <kalbeabbas142@gmail.com>
2020-05-03 07:41:59 +0500

    SG34 sensor

---
+++ sg34.ts
@@ -0,0 +1,36 @@
+namespace sg34{
+	//% shim=max30105::setup_sg34
+	function setup(): void
+	{
+	}
+	
+	//%block="SG34 get Red"
+	//%shim=max30105::getRedSG34
+	export function getRed():number
+	{
+		return 1
+	}
+	
+	//%block="SG34 get Green"
+	//%shim=max30105::getGreenSG34
+	export function getGreen():number
+	{
+		return 1
+	}
+	
+	//%block="SG34 get IR"
+	//%shim=max30105::getIRSG34
+	export function getBlue():number
+	{
+		return 1
+	}
+	
+	//%block="SG34 get temperature"
+	//%shim=max30105::getTempSG34
+	export function getTemp():number
+	{
+		return 1
+	}
+	
+	setup()
+}
