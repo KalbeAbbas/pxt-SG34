8363be3ec4870ac30dfb22f55c467cc62817345a
KalbeAbbas <kalbeabbas@142@gmail.com>
2020-05-04 13:09:12 +0500

    new SG34

--- MAX30105.cpp
+++ MAX30105.cpp
@@ -815,7 +815,11 @@ namespace max30105{
 		
 		maxim_heart_rate_and_oxygen_saturation(sg34->irBuffer, 100, sg34->redBuffer, &(sg34->spo2), &(sg34->validSPO2), &(sg34->heartRate), &(sg34->validHeartRate));*/
 		
-		return sg34->setup(60, 4, 2, 400, 411, 4096);
+		sg34->setup();
+		sg34->setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+		sg34->setPulseAmplitudeGreen(0); //Turn off Green 
+		
+		return 1;
 	}
 		
 	
